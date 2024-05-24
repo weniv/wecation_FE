4c56e584ee8062e763b4fed7cc1b3c47be38e7d4
baekg110 <baekg.dev@gmail.com>
2024-05-24 14:16:06 +0900

    :memo: Edit: 라우팅 수정

--- src/routes/Routers.jsx
+++ src/routes/Routers.jsx
@@ -15,16 +15,16 @@ const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/wecation_FE">
+        <Route path="/wecation_FE/">
           <Route element={<NavLayout />}>
-            <Route path="/" element={<MapPage />} />
-            <Route path="/community" element={<CommunityPage />} />
-            <Route path="/place/:id" element={<PlacePage />} />
-            <Route path="/user" element={<UserPage />} />
+            <Route index element={<MapPage />} />
+            <Route path="community" element={<CommunityPage />} />
+            <Route path="place/:id" element={<PlacePage />} />
+            <Route path="user" element={<UserPage />} />
           </Route>
           <Route element={<LogoLayout />}>
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/signup" element={<SignupPage />} />
+            <Route path="login" element={<LoginPage />} />
+            <Route path="signup" element={<SignupPage />} />
           </Route>
           <Route path="*" element={<NotFoundPage />} />
         </Route>
